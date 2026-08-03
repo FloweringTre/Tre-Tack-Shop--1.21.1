cb975fc9e4e2cac1750b1c85670ca97533adb1de
kyraltre <123285225+FloweringTre@users.noreply.github.com>
2026-08-02 18:12:30 -0700

    award items ported - bug to not drop on break though

--- src/main/java/com/kyraltre/tretackshop/block/TackShopBlocks.java
+++ src/main/java/com/kyraltre/tretackshop/block/TackShopBlocks.java
@@ -1,136 +1,118 @@
 package com.kyraltre.tretackshop.block;
 
-import com.alaharranhonor.swem.forge.blocks.ConeBase;
+import com.alaharranhonor.swem.block.ConeBase;
 import com.kyraltre.tretackshop.TreTackShop;
 import com.kyraltre.tretackshop.block.custom.*;
 import com.kyraltre.tretackshop.registry.AwardShopItems;
 import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegistryObject;
+import net.minecraft.world.level.block.state.properties.*;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredBlock;
+import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredRegister;
 
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceKey;
+
+import java.util.function.Function;
 import java.util.function.Supplier;
 
 public class TackShopBlocks {
-    public static final DeferredRegister<Block> BLOCKS =
-            DeferredRegister.create(ForgeRegistries.BLOCKS, TreTackShop.MOD_ID);
+    //public static final DeferredRegister.Blocks REGISTRY = DeferredRegister.createBlocks("tretackshop");
+    public static final DeferredRegister.Blocks BLOCKS =
+            DeferredRegister.createBlocks(TreTackShop.MOD_ID);
 
-//    public static final RegistryObject<Block> CONE_1 = registerBlock("cone_1",
+//    public static final DeferredBlock<Block> CONE_1 = registerBlock("cone_1",
 //            () -> new ConeBase());
-//    public static final RegistryObject<Block> CONE_2 = registerBlock("cone_2",
+//    public static final DeferredBlock<Block> CONE_2 = registerBlock("cone_2",
 //            () -> new ConeBase());
-//    public static final RegistryObject<Block> CONE_3 = registerBlock("cone_3",
+//    public static final DeferredBlock<Block> CONE_3 = registerBlock("cone_3",
 //            () -> new ConeBase());
-//    public static final RegistryObject<Block> CONE_4 = registerBlock("cone_4",
+//    public static final DeferredBlock<Block> CONE_4 = registerBlock("cone_4",
 //            () -> new ConeBase());
-//    public static final RegistryObject<Block> CONE_5 = registerBlock("cone_5",
+//    public static final DeferredBlock<Block> CONE_5 = registerBlock("cone_5",
 //            () -> new ConeBase());
-//    public static final RegistryObject<Block> CONE_6 = registerBlock("cone_6",
+//    public static final DeferredBlock<Block> CONE_6 = registerBlock("cone_6",
 //            () -> new ConeBase());
-//    public static final RegistryObject<Block> CONE_7 = registerBlock("cone_7",
+//    public static final DeferredBlock<Block> CONE_7 = registerBlock("cone_7",
 //            () -> new ConeBase());
-//    public static final RegistryObject<Block> CONE_8 = registerBlock("cone_8",
+//    public static final DeferredBlock<Block> CONE_8 = registerBlock("cone_8",
 //            () -> new ConeBase());
-//    public static final RegistryObject<Block> CONE_9 = registerBlock("cone_9",
+//    public static final DeferredBlock<Block> CONE_9 = registerBlock("cone_9",
 //            () -> new ConeBase());
-//    public static final RegistryObject<Block> CONE_10 = registerBlock("cone_10",
+//    public static final DeferredBlock<Block> CONE_10 = registerBlock("cone_10",
 //            () -> new ConeBase());
-//    public static final RegistryObject<Block> CONE_11 = registerBlock("cone_11",
+//    public static final DeferredBlock<Block> CONE_11 = registerBlock("cone_11",
 //            () -> new ConeBase());
-//    public static final RegistryObject<Block> CONE_12 = registerBlock("cone_12",
+//    public static final DeferredBlock<Block> CONE_12 = registerBlock("cone_12",
 //            () -> new ConeBase());
-//    public static final RegistryObject<Block> CONE_13 = registerBlock("cone_13",
+//    public static final DeferredBlock<Block> CONE_13 = registerBlock("cone_13",
 //            () -> new ConeBase());
-//    public static final RegistryObject<Block> CONE_14 = registerBlock("cone_14",
+//    public static final DeferredBlock<Block> CONE_14 = registerBlock("cone_14",
 //            () -> new ConeBase());
-//    public static final RegistryObject<Block> CONE_MONARCH = registerBlock("cone_monarch",
+//    public static final DeferredBlock<Block> CONE_MONARCH = registerBlock("cone_monarch",
 //            () -> new ConeBase());
-//    public static final RegistryObject<Block> CONE_MORPHO = registerBlock("cone_morpho",
+//    public static final DeferredBlock<Block> CONE_MORPHO = registerBlock("cone_morpho",
 //            () -> new ConeBase());
-    public static final RegistryObject<Block> CONE_RAINBOW = registerBlock("cone_rainbow",
-            () -> new ConeBase());
+    public static final DeferredBlock<ConeBase> CONE_RAINBOW = registerBlock("cone_rainbow",
+            (properties -> new ConeBase()));
 
-    //REMOVED BLOCK pumpkin stature, only item frame variant now
-//    public static final RegistryObject<Block> PUMPKIN_GRAND = registerBlock("pumpkin_grand",
-//            () -> new PumpkinTrophy(BlockBehaviour.Properties.copy(Blocks.PUMPKIN).noOcclusion().strength(1.0F)
-//            ));
-//
-//    public static final RegistryObject<Block> PUMPKIN_RESERVE = registerBlock("pumpkin_reserve",
-//            () -> new PumpkinTrophy(BlockBehaviour.Properties.copy(Blocks.PUMPKIN).noOcclusion().strength(1.0F)
-//            ));
+    public static final DeferredBlock<PumpkinTrophy> PUMPKIN_GRAND = registerBlock("pumpkin_grand",
+            (properties -> new PumpkinTrophy(properties.strength(1.0F))));
+    public static final DeferredBlock<PumpkinTrophy> PUMPKIN_RESERVE= registerBlock("pumpkin_reserve",
+            (properties -> new PumpkinTrophy(properties.strength(1.0F))));
 
-    public static final RegistryObject<Block> PUMPKIN_GRAND = registerBlock("pumpkin_grand",
-            () -> new PumpkinTrophy(BlockBehaviour.Properties.copy(Blocks.IRON_BLOCK).noOcclusion().strength(1.0F)
-            ));
+    public static final DeferredBlock<SnowflakeTrophy> SNOWFLAKE_GRAND = registerBlock("snowflake_grand",
+            (properties -> new SnowflakeTrophy(properties.strength(1.0F))));
+    public static final DeferredBlock<SnowflakeTrophy> SNOWFLAKE_RESERVE = registerBlock("snowflake_reserve",
+            (properties -> new SnowflakeTrophy(properties.strength(1.0F))));
 
-    public static final RegistryObject<Block> PUMPKIN_RESERVE = registerBlock("pumpkin_reserve",
-            () -> new PumpkinTrophy(BlockBehaviour.Properties.copy(Blocks.IRON_BLOCK).noOcclusion().strength(1.0F)
-            ));
+    public static final DeferredBlock<HeartTrophy> HEART_GRAND = registerBlock("heart_grand",
+            (properties -> new HeartTrophy(properties.strength(1.0F))));
+    public static final DeferredBlock<HeartTrophy> HEART_RESERVE = registerBlock("heart_reserve",
+            (properties -> new HeartTrophy(properties.strength(1.0F))));
 
-    public  static final RegistryObject<Block> SNOWFLAKE_GRAND =registerBlock("snowflake_grand", () ->
-            new SnowflakeTrophy(BlockBehaviour.Properties.copy(Blocks.IRON_BLOCK).noOcclusion().strength(1.0F)
-            ));
-    public static final RegistryObject<Block> SNOWFLAKE_RESERVE = registerBlock("snowflake_reserve",
-            () -> new SnowflakeTrophy(BlockBehaviour.Properties.copy(Blocks.IRON_BLOCK).noOcclusion().strength(1.0F)
-            ));
-    public  static final RegistryObject<Block> HEART_GRAND =registerBlock("heart_grand", () ->
-            new HeartTrophy(BlockBehaviour.Properties.copy(Blocks.IRON_BLOCK).noOcclusion().strength(1.0F)
-            ));
-    public static final RegistryObject<Block> HEART_RESERVE = registerBlock("heart_reserve",
-            () -> new HeartTrophy(BlockBehaviour.Properties.copy(Blocks.IRON_BLOCK).noOcclusion().strength(1.0F)
-            ));
+    public static final DeferredBlock<CloverTrophy> CLOVER_GRAND = registerBlock("clover_grand",
+            (properties -> new CloverTrophy(properties.strength(1.0F))));
+    public static final DeferredBlock<CloverTrophy> CLOVER_RESERVE = registerBlock("clover_reserve",
+            (properties -> new CloverTrophy(properties.strength(1.0F))));
 
-    public static final RegistryObject<Block> CLOVER_GRAND = registerBlock("clover_grand", () ->
-            new CloverTrophy(BlockBehaviour.Properties.copy(Blocks.IRON_BLOCK).requiresCorrectToolForDrops().noOcclusion().strength(1.0F)
-            ));
-    public static final RegistryObject<Block> CLOVER_RESERVE = registerBlock("clover_reserve",
-            () -> new CloverTrophy(BlockBehaviour.Properties.copy(Blocks.IRON_BLOCK).requiresCorrectToolForDrops().noOcclusion().strength(1.0F)
-            ));
+    public static final DeferredBlock<EggTrophy> EGG_TROPHY_THICK_1 = registerBlock("e_g_g_trophy_thick_1",
+            (properties -> new EggTrophy(properties.strength(1.0F))));
+    public static final DeferredBlock<EggTrophy> EGG_TROPHY_THICK_2 = registerBlock("e_g_g_trophy_thick_2",
+            (properties -> new EggTrophy(properties.strength(1.0F))));
+    public static final DeferredBlock<EggTrophy> EGG_TROPHY_THICK_3 = registerBlock("e_g_g_trophy_thick_3",
+            (properties -> new EggTrophy(properties.strength(1.0F))));
+    public static final DeferredBlock<EggTrophy> EGG_TROPHY_THIN_1 = registerBlock("e_g_g_trophy_thin_1",
+            (properties -> new EggTrophy(properties.strength(1.0F))));
+    public static final DeferredBlock<EggTrophy> EGG_TROPHY_THIN_2 = registerBlock("e_g_g_trophy_thin_2",
+            (properties -> new EggTrophy(properties.strength(1.0F))));
+    public static final DeferredBlock<EggTrophy> EGG_TROPHY_THIN_3 = registerBlock("e_g_g_trophy_thin_3",
+            (properties -> new EggTrophy(properties.strength(1.0F))));
+    public static final DeferredBlock<EggTrophy> EGG_TROPHY_CHEV_1 = registerBlock("e_g_g_trophy_chev_1",
+            (properties -> new EggTrophy(properties.strength(1.0F))));
+    public static final DeferredBlock<EggTrophy> EGG_TROPHY_CHEV_2 = registerBlock("e_g_g_trophy_chev_2",
+            (properties -> new EggTrophy(properties.strength(1.0F))));
+    public static final DeferredBlock<EggTrophy> EGG_TROPHY_CHEV_3 = registerBlock("e_g_g_trophy_chev_3",
+            (properties -> new EggTrophy(properties.strength(1.0F))));
 
-    public static final RegistryObject<Block> EGG_TROPHY_THICK_1 = registerBlock("e_g_g_trophy_thick_1",
-            () -> new EggTrophy(BlockBehaviour.Properties.copy(Blocks.IRON_BLOCK).requiresCorrectToolForDrops().noOcclusion().strength(1.0F)
-            ));
-    public static final RegistryObject<Block> EGG_TROPHY_THICK_2 = registerBlock("e_g_g_trophy_thick_2",
-            () -> new EggTrophy(BlockBehaviour.Properties.copy(Blocks.IRON_BLOCK).requiresCorrectToolForDrops().noOcclusion().strength(1.0F)
-            ));
-    public static final RegistryObject<Block> EGG_TROPHY_THICK_3 = registerBlock("e_g_g_trophy_thick_3",
-            () -> new EggTrophy(BlockBehaviour.Properties.copy(Blocks.IRON_BLOCK).requiresCorrectToolForDrops().noOcclusion().strength(1.0F)
-            ));
 
-    public static final RegistryObject<Block> EGG_TROPHY_THIN_1 = registerBlock("e_g_g_trophy_thin_1",
-            () -> new EggTrophy(BlockBehaviour.Properties.copy(Blocks.IRON_BLOCK).requiresCorrectToolForDrops().noOcclusion().strength(1.0F)
-            ));
-    public static final RegistryObject<Block> EGG_TROPHY_THIN_2 = registerBlock("e_g_g_trophy_thin_2",
-            () -> new EggTrophy(BlockBehaviour.Properties.copy(Blocks.IRON_BLOCK).requiresCorrectToolForDrops().noOcclusion().strength(1.0F)
-            ));
-    public static final RegistryObject<Block> EGG_TROPHY_THIN_3 = registerBlock("e_g_g_trophy_thin_3",
-            () -> new EggTrophy(BlockBehaviour.Properties.copy(Blocks.IRON_BLOCK).requiresCorrectToolForDrops().noOcclusion().strength(1.0F)
-            ));
-
-    public static final RegistryObject<Block> EGG_TROPHY_CHEV_1 = registerBlock("e_g_g_trophy_chev_1",
-            () -> new EggTrophy(BlockBehaviour.Properties.copy(Blocks.IRON_BLOCK).requiresCorrectToolForDrops().noOcclusion().strength(1.0F)
-            ));
-    public static final RegistryObject<Block> EGG_TROPHY_CHEV_2 = registerBlock("e_g_g_trophy_chev_2",
-            () -> new EggTrophy(BlockBehaviour.Properties.copy(Blocks.IRON_BLOCK).requiresCorrectToolForDrops().noOcclusion().strength(1.0F)
-            ));
-    public static final RegistryObject<Block> EGG_TROPHY_CHEV_3 = registerBlock("e_g_g_trophy_chev_3",
-            () -> new EggTrophy(BlockBehaviour.Properties.copy(Blocks.IRON_BLOCK).requiresCorrectToolForDrops().noOcclusion().strength(1.0F)
-            ));
-
-    private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block) {
-        RegistryObject<T> toReturn = BLOCKS.register(name, block);
+    private static <T extends Block> DeferredBlock<T> registerBlock(String name, Function<BlockBehaviour.Properties, T> function) {
+        DeferredBlock<T> toReturn = BLOCKS.registerBlock(name, function);
         registerBlockItem(name, toReturn);
         return toReturn;
     }
 
-    private static <T extends Block> RegistryObject<Item> registerBlockItem(String name, RegistryObject<T> block) {
-        return AwardShopItems.REGISTRY.register(name, () -> new BlockItem(block.get(), new Item.Properties()));
+    private static <T extends Block> void registerBlockItem(String name, DeferredBlock<T> block) {
+        AwardShopItems.REGISTRY.registerItem(name, (properties) -> new BlockItem(block.get(), new Item.Properties()));
     }
+    //private static <T extends Block> DeferredBlock<Item> registerBlockItem(String name, DeferredBlock<T> block) {
+    //    return AwardShopItems.REGISTRY.register(name, () -> new BlockItem(block.get(), new Item.Properties()));
+    //}
 
     public static void register(IEventBus eventBus){
         BLOCKS.register(eventBus);
